8e7649090dda12e0eba96480367e8b11ab23c12d
Abul Fajle Azad <abulfajle405@gmail.com>
2022-11-05 02:39:54 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react'
 import './App.css';
 
+let adjustInterval = undefined;
+
+
 function App() {
+  const [watch,setWatch]=useState(0);
+  const [started,setStarted]=useState(false)
+
+
+  const startTime=()=>{
+    adjustInterval=setInterval(()=>{
+      setWatch((sec)=>sec+1)
+
+    },1000)
+    setStarted(true)
+  }
+
+  const stopTime =()=>{
+    clearInterval(adjustInterval)
+    setStarted(false)
+
+  }
+
+  const resetTime = ()=>{
+    setWatch(0)
+    clearInterval(adjustInterval)
+    setStarted(false)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main" typeof='card'>
+        <h1 className='heading'>Stop Watch</h1>
+        <h2> {watch } </h2>
+        <div className="btn">
+          <button disabled={started} className="start" onClick={startTime}>Start</button>
+          <button className="stop" onClick={stopTime}>Stop</button>
+          <button className="reset"onClick={resetTime}>Reset</button>
+        </div>
+      </div>
+      <h6 className='name'>Projected Abul Azad</h6>
     </div>
   );
 }
